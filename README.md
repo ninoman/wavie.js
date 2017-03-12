9243fef4e80e75250ac83375c5ef1642ffcb30e2
Arman <arm.ninoyan@gmail.com>
2017-03-12 23:22:44 +0400

    Let it begin

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# wavie.js
+We call it
+
+# Wavie.js
+
+### Make your own canvas tags with this easy to use JavaScript library. 
+
+------------------------------
+### `Make your own`
+
+<a href="http://wavieJS.github.io" target="_blank"><img src="https://waviejs.github.com/screen-shot.png" alt="Wavie JS" /></a>
+
+
+
+-------------------------------
+### `Usage`
+
+How we say, it's very easy to use WavieJS in your project
+
+Load wavier.js <br/>
+
+**index.html**
+```html
+<script src="wavie.js"></script>
+```
+
+Run wavie function 
+**app.js**
+```javascript
+wavie('yourCanvasTagId', 'JSON config path');
+```
+
+You even can do this
+**app.js**
+```javascript
+wavie(yourCanvasDOM, configObject);
+```
+
+**wavie.json**
+```javascript
+{
+    "ballSize": 1,
+    "speed": 1,
+
+    "phaseRefX": 0.12,
+    "phaseRefY": 0.12,
+
+    "radiusX": 15,
+    "radiusY": 15,
+
+    "densityX": 6,
+    "densityY": 6,
+
+    "backgroundFrom":"#1e174a",
+    "backgroundTo":"#000000",
+    "degree":62,
+    "ballColor": "#837ea2"
+}
+```
+
+-------------------------------
+
+### `Options`
+
+For 
+
+-------------------------------
+
+
---
+++ wavie.js
@@ -0,0 +1,87 @@
+function wavie(canvas, config) {
+
+    if (typeof canvas == 'string') {
+        canvas = document.getElementById(canvas);
+    }
+
+    var t = 0;
+
+    var devicePixelRatio = window.devicePixelRatio || 1;
+    canvas.width = devicePixelRatio * canvas.offsetWidth;
+    canvas.height = devicePixelRatio * canvas.offsetHeight;
+
+    context = canvas.getContext('2d');
+    context.scale(devicePixelRatio, devicePixelRatio);
+
+    if (typeof config == 'string') {
+        getJSON(config, function (err, jsonConfig) {
+            if (err) {
+                console.log(err);
+            }
+            if (!jsonConfig) {
+                console.log("Empty config");
+            }
+            config = jsonConfig;
+            requestAnimationFrame(render);
+        })
+    } else {
+        requestAnimationFrame(render);
+    }
+
+
+    function render() {
+
+        canvas.style.background = 'linear-gradient(%1deg, %2, %3)'
+            .replace('%1', config.degree)
+            .replace('%2', config.backgroundFrom)
+            .replace('%3', config.backgroundTo);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        var padding = 1;
+        var timeStep = 0.006;
+
+        var stepX = 100 / config.densityX;
+        var stepY = 100 / config.densityY;
+
+        var ballsNumX = (1 + padding) * canvas.width / stepX;
+        var ballsNumY = (1 + padding) * canvas.height / stepY;
+
+        var offsetX = -canvas.width / 2 * padding;
+        var offsetY = -canvas.height / 2 * padding;
+
+        for (var i = 0; i < ballsNumY; ++i) {
+            for (var j = 0; j < ballsNumX; ++j) {
+
+                var centerX = stepX * j + offsetX;
+                var centerY = stepY * i + offsetY;
+                var phase = Math.PI * (config.phaseRefY * i + config.phaseRefX * j + t);
+
+                var positionX = centerX + config.radiusX * Math.sin(phase);
+                var positionY = centerY + config.radiusY * Math.cos(phase);
+
+                context.beginPath();
+                context.arc(positionX, positionY, config.ballSize, 0, 2 * Math.PI, false);
+                context.fillStyle = config.ballColor;
+                context.fill();
+            }
+        }
+
+        t += timeStep * config.speed;
+
+        requestAnimationFrame(render);
+    }
+
+    function getJSON(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", url, true);
+        xhr.responseType = "json";
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                callback(null, xhr.response);
+            } else {
+                callback(status);
+            }
+        };
+        xhr.send();
+    };
+};
